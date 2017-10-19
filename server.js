7c93b9471657f3a5b8d9b7c62af91f7e24932595
Brandon <idhere703@gmail.com>
2017-10-18 21:40:00 -0400

    Added server

--- server.js
+++ server.js
@@ -13,6 +13,4 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 // Serve the files on port 3000.
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!\n');
-});
+app.listen(3000, function() {});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-    entry: './src/app.js',
+    entry: {
+        app: './src/app.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist'
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/'
     }
