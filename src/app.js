a1a4a0a22295ead4ca4887b703a2376d87711609
Brandon <idhere703@gmail.com>
2017-10-31 23:25:35 -0400

    Fade in function.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
 import PouchDB from 'pouchdb-browser';
 import './app.css';
 const db = new PouchDB('potato_database');
+
+
+const fadeIn = (msgSelector) => {
+    //get the welcome msg element
+    const $all_msg = $(msgSelector);
+    //get a list of letters from the welcome text
+    const $wordList = $(msgSelector).text().split("");
+    //clear the welcome text msg
+    $(msgSelector).text("");
+    //loop through the letters in the $wordList array
+    $.each($wordList, (idx, elem) => {
+        //create a span for the letter and set opacity to 0
+        const newEL = $("<span/>").text(elem).css({
+            opacity: 0
+        });
+        //append it to the welcome message
+        newEL.appendTo($all_msg);
+        //set the delay on the animation for this element
+        newEL.delay(idx * 70);
+        //animate the opacity back to full 1
+        newEL.animate({
+            opacity: 1
+        }, 1100);
+    });
+
+};
